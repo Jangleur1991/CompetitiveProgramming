ed38a063b218aaa5a0eb7bf91bc8e1cbdd56c66e
Jangleur1991 <Jangleur@web.de>
2022-07-07 12:42:04 +0100

    ConvertBinaryToInteger commentar added

--- LeetCodeChallengesJava/src/main/java/linkedlist/ConvertBinaryToInteger.java
+++ LeetCodeChallengesJava/src/main/java/linkedlist/ConvertBinaryToInteger.java
@@ -1,5 +1,10 @@
 package linkedlist;
 
+/**
+ * Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+ *
+ * Return the decimal value of the number in the linked list.
+ */
 public class ConvertBinaryToInteger {
     public int getDecimalValue(ListNode head) {
         int result = 0;
