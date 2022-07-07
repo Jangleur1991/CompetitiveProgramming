adf3eb998ce07944622a5d3631758cdbad4dd8cc
Jangleur1991 <Jangleur@web.de>
2022-07-07 15:41:50 +0100

    Created own implementation of LinkedList.

---
+++ LeetCodeChallengesJava/src/main/java/linkedlist/MyLinkedList.java
@@ -0,0 +1,58 @@
+package linkedlist;
+
+public class MyLinkedList<E> {
+    MyNode<E> head;
+
+    public MyNode<E> getHead() {
+        return this.head;
+    }
+
+    public void addElement(E element) {
+        if (null == head) {
+            head = new MyNode<>(element, null);
+        } else {
+            MyNode<E> currentNode = head;
+            while(null != currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = new MyNode<>(element, null);
+        }
+    }
+
+    public void deleteElement(E element) {
+        if (null != head) {
+            MyNode<E> currentNode = head;
+            while(null != currentNode.next) {
+                if (element == currentNode.next.element) {
+                    currentNode.next = currentNode.next.next;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    public void printElements() {
+        if (null != head) {
+            MyNode<E> currentNode = head;
+            while (null != currentNode) {
+                System.out.println(currentNode.element);
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    public static class MyNode<E> {
+        E element;
+        MyNode<E> next;
+
+        public MyNode(E element, MyNode<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Element: " + element;
+        }
+    }
+}
