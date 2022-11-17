bf1ac74d1ac7f8acb3870f4e9bf9c6e42dc794ff
Jangleur1991 <Jangleur@web.de>
2022-11-17 07:05:05 +0000

    delete a node of a linkedlist

--- LeetCodeChallengesJava/src/main/java/linkedlist/DeleteNodeLinkedList.java
+++ LeetCodeChallengesJava/src/main/java/linkedlist/DeleteNodeLinkedList.java
@@ -2,6 +2,19 @@ package linkedlist;
 
 import linkedlist.MyLinkedList.MyNode;
 
+/**
+ * There is a singly-linked list head and we want to delete a node node in it.
+ *
+ * You are given the node to be deleted node. You will not be given access to the first node of head.
+ *
+ * All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+ *
+ * Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+ *
+ * Input: head = [4,5,1,9], node = 5
+ * Output: [4,1,9]
+ * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+ */
 public class DeleteNodeLinkedList {
 
 
@@ -11,10 +24,10 @@ public class DeleteNodeLinkedList {
         myLinkedList.addElement(5);
         myLinkedList.addElement(1);
         myLinkedList.addElement(9);
-        myLinkedList.printElements();
         MyNode<Integer> node = myLinkedList.getNodeBy(5);
-        System.out.println();
+        System.out.println(node.element);
         deleteNode(node);
+        System.out.println();
         myLinkedList.printElements();
     }
 
