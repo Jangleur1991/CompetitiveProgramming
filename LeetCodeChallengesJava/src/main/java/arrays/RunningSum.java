8c529eb9f7e461388db0ab0fd5ce1fc751ca3823
Jangleur1991 <Jangleur@web.de>
2022-11-30 16:17:32 +0000

    RunningSum recursive solution

--- LeetCodeChallengesJava/src/main/java/arrays/RunningSum.java
+++ LeetCodeChallengesJava/src/main/java/arrays/RunningSum.java
@@ -10,17 +10,29 @@ import java.util.Arrays;
 public class RunningSum {
     public static void main(String[] args) {
         int[] nums = {1,1,1,1,1};
-        System.out.println(Arrays.toString(runningSum(nums)));
+        System.out.println(Arrays.toString(runningSumRec(nums)));
+    }
+
+    private static int[] runningSumRec(int[] nums) {
+        return sum(nums, 1);
+    }
+
+    private static int[] sum(int[] nums, int counter) {
+        return (counter == nums.length)
+                ? nums
+                : sum(calcValue(nums, counter), ++counter);
+    }
+
+    private static int[] calcValue(int[] nums, int counter) {
+        nums[counter] += nums[counter-1];
+        return nums;
     }
 
     private static int[] runningSum(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        int temp = 0;
-        for (int i = 0; i < n; i++) {
-            temp += nums[i];
-            ans[i] = temp;
+        for (int i = 1; i < n; i++) {
+            nums[i] = nums[i-1] + nums[i];
         }
-        return ans;
+        return nums;
     }
 }
