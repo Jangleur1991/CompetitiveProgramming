fb39f79f3670e99a45d525bd2c4903c7e95f0fa6
Jangleur1991 <Jangleur@web.de>
2023-09-18 06:57:13 +0100

    CountPairs using binary search

--- LeetCodeChallengesJava/src/main/java/techniques/twoPointer/CountPairs.java
+++ LeetCodeChallengesJava/src/main/java/techniques/twoPointer/CountPairs.java
@@ -12,9 +12,11 @@ import java.util.List;
  */
 public class CountPairs {
     public static void main(String[] args) {
-        List<Integer> nums1 = new ArrayList<>(List.of(-1,1,2,3,1));
-        List<Integer> nums2 = new ArrayList<>(List.of(-6,2,5,-2,-7,-1,3));
+        List<Integer> nums1 = new ArrayList<>(List.of(-1, 1, 2, 3, 1));
+        List<Integer> nums2 = new ArrayList<>(List.of(-6, 2, 5, -2, -7, -1, 3));
 
+        // -1, 1, 1, 2, 3
+        //
         int target1 = 2;
         int target2 = -2;
 
@@ -22,19 +24,42 @@ public class CountPairs {
         System.out.println(countPairs(nums2, target2)); //10
     }
 
+//    private static int countPairs(List<Integer> nums, int target) {
+//        Collections.sort(nums);
+//        int result = 0;
+//        int left = 0;
+//        int right = nums.size() - 1;
+//        while (left < right) {
+//            int currentSum = nums.get(left) + nums.get(right);
+//            if (currentSum < target) {
+//                result += right - left;
+//                left++;
+//            } else {
+//                right--;
+//            }
+//        }
+//        return result;
+//    }
+
     private static int countPairs(List<Integer> nums, int target) {
         Collections.sort(nums);
         int result = 0;
-        int left = 0;
-        int right = nums.size()-1;
-        while (left < right) {
-            int currentSum = nums.get(left) + nums.get(right);
-            if (currentSum < target) {
-                result += right - left;
-                left++;
-            } else {
-                right--;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int left = 0;
+            int right = i-1;
+            int currentNumberOfPairs = i;
+
+            while (left <= right) {
+                int midElementIndex = (left+right)/2;
+                if (nums.get(midElementIndex) >= target - nums.get(i)) {
+                    currentNumberOfPairs = midElementIndex;
+                    right = midElementIndex-1;
+                } else {
+                    left = midElementIndex+1;
+                }
             }
+            result +=currentNumberOfPairs;
         }
         return result;
     }
