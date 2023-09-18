14c5a0543c9d5f2757a274ae9e28774acfedd61d
Jangleur1991 <Jangleur@web.de>
2023-09-18 06:32:36 +0100

    containsDuplicate solution using a set

--- LeetCodeChallengesJava/src/main/java/warmup/ContainsDuplicate.java
+++ LeetCodeChallengesJava/src/main/java/warmup/ContainsDuplicate.java
@@ -1,6 +1,6 @@
 package warmup;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Given an integer array nums, return true if any value appears at least twice in the array,
@@ -9,19 +9,31 @@ import java.util.Arrays;
 public class ContainsDuplicate {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 1};
-        int[] nums2 = {1,2,3,4};
-        int[] nums3 = {1,1,1,3,3,4,3,2,4,2};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
 
         System.out.println(containsDuplicate(nums1));
         System.out.println(containsDuplicate(nums2));
         System.out.println(containsDuplicate(nums3));
     }
 
+//    private static boolean containsDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            if (nums[i] == nums[i + 1])
+//                return true;
+//        }
+//        return false;
+//    }
+
     private static boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i+1])
+        Set<Object> set = new HashSet<>();
+        for (int num: nums) {
+            if (set.contains(num)) {
                 return true;
+            } else {
+                set.add(num);
+            }
         }
         return false;
     }
