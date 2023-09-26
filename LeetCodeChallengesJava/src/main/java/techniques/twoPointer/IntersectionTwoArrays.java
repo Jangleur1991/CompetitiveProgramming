5346cc427e23cc61d87edf02b42ee8af55f72a4b
Jangleur1991 <Jangleur@web.de>
2023-09-26 07:22:10 +0100

    IntersectionTwoArrays solved two pointers

---
+++ LeetCodeChallengesJava/src/main/java/techniques/twoPointer/IntersectionTwoArrays.java
@@ -0,0 +1,48 @@
+package techniques.twoPointer;
+
+import java.util.*;
+import java.util.function.Function;
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be
+ * unique and you may return the result in any order.
+ */
+public class IntersectionTwoArrays {
+    public static void main(String[] args) {
+        int[] nums11 = new int[]{1,2,2,1};
+        int[] nums21 = new int[]{2,2};
+
+        int[] nums12 = new int[]{4,9,5};
+        int[] nums22 = new int[]{9,4,9,8,4};
+
+        int[] nums13 = new int[]{1};
+        int[] nums23 = new int[]{1};
+
+        System.out.println(Arrays.toString(intersection(nums11, nums21))); // 2
+        System.out.println(Arrays.toString(intersection(nums12, nums22))); // 4 9
+        System.out.println(Arrays.toString(intersection(nums13, nums23))); // 1
+
+    }
+
+    private static int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> result = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j])
+                i++;
+            else if (nums2[j] < nums1[i])
+                j++;
+            else {
+                if (result.isEmpty() || result.get(result.size()-1) != nums1[i]) {
+                    result.add(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
