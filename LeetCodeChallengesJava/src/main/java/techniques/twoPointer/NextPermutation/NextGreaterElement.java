6d7db90481b95b18fb7dc129ca9c166ff52dc35a
Jangleur1991 <Jangleur@web.de>
2023-10-23 06:57:52 +0100

    NextGreaterElement prepared

---
+++ LeetCodeChallengesJava/src/main/java/techniques/twoPointer/NextPermutation/NextGreaterElement.java
@@ -0,0 +1,23 @@
+package techniques.twoPointer.NextPermutation;
+
+/**
+ * Given a positive integer n, find the smallest integer which has exactly the same digits existing in the integer n
+ * and is greater in value than n. If no such positive integer exists, return -1.
+ *
+ * Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it does not fit in
+ * 32-bit integer, return -1.
+ */
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = 21;
+
+        System.out.println(nextGreaterElement(n1));
+        System.out.println(nextGreaterElement(n2));
+    }
+
+    private static int nextGreaterElement(int n) {
+        return 0;
+    }
+
+}
