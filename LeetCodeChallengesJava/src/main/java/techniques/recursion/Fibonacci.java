865b5b565c721b73d46781a39574d7d34033c9c9
Jangleur1991 <Jangleur@web.de>
2023-11-10 06:29:54 +0000

    fibonacci bottom up

--- LeetCodeChallengesJava/src/main/java/techniques/recursion/Fibonacci.java
+++ LeetCodeChallengesJava/src/main/java/techniques/recursion/Fibonacci.java
@@ -14,16 +14,29 @@ public class Fibonacci {
         System.out.println(fib(n3));
     }
 
-    private static int fib(int n) {
-        return fibHelper(n, new int[n+1]);
-    }
+//    private static int fib(int n) {
+//        return fibHelper(n, new int[n+1]);
+//    }
+//
+//    private static int fibHelper(int n, int[] memo)
+//    {
+//        if (n == 0) return 0;
+//        if (n <= 2) return 1;
+//        if (memo[n] != 0) return memo[n];
+//        memo[n] = fibHelper(n-1, memo) + fibHelper(n-2, memo);
+//        return memo[n];
+//    }
 
-    private static int fibHelper(int n, int[] memo)
-    {
-        if (n == 0) return 0;
-        if (n <= 2) return 1;
-        if (memo[n] != 0) return memo[n];
-        memo[n] = fibHelper(n-1, memo) + fibHelper(n-2, memo);
-        return memo[n];
+    private static int fib(int n) {
+        if (n<1) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int f1 = 1, f2 = 1, result = 2;
+        for (int i = 4; i <= n; i++) {
+            f1 = f2;
+            f2 = result;
+            result = f1 + f2;
+        }
+        return result;
     }
 }
