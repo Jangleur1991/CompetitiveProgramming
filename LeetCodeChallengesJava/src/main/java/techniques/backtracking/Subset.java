b77bc367d7a9fa55fa14826dc79fa63c07267f69
Jan Albert <jan.albert@pulselive.com>
2023-11-18 09:24:22 +0000

    Subset solved using bitmasks

--- LeetCodeChallengesJava/src/main/java/techniques/backtracking/Subset.java
+++ LeetCodeChallengesJava/src/main/java/techniques/backtracking/Subset.java
@@ -23,6 +23,8 @@ public class Subset
         subsets( nums );
         System.out.println(subsets( nums1 )); //[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
         System.out.println(subsets( nums2 )); //[[],[0]]
+
+        bitmasksLengthN(3);
     }
 
     // private static List<List<Integer>> subsets(int[] nums) {
@@ -64,5 +66,62 @@ public class Subset
             subset.remove( subset.size() - 1 );
             generateSubsets( nums, subset, index+1, result );
         }
+    }
+
+    //Two ways to generat bitmasks of a specific length
+    private static void bitmasksLengthN(int n)
+    {
+        //
+        int nthBit = 1 << n; // == 2^n
+        for ( int i = 0; i < nthBit; i++ )
+        {
+            //i | nthBit sets the nthBit in front of the binary
+            // which can be 0 or 1 but it guarantees the length of n+1 of the bitmask!
+            //We then remove the first bit so we get only bitmasks of length n!
+            String bitMask = Integer.toBinaryString( i | nthBit ).substring( 1 );
+            System.out.println(bitMask);
+        }
+
+        System.out.println(" ");
+        //Similar idea but we generate the bitmasks between 2^n and 2^n+1 - 1.
+        // These bitmask are guaranteed to have a length of n+1
+        // Again we remove the first bit => we get all bitmasks of length n!
+        for ( int i = (int) Math.pow( 2,  n); i < (int) Math.pow( 2,  n+1); i++ )
+        {
+            String bitMask = Integer.toBinaryString( i ).substring( 1 );
+            System.out.println(bitMask);
+        }
+    }
+
+    /**
+     * Generate all possible binary bitmasks of length n.
+     *
+     * Map a subset to each bitmask:
+     * 1 on the ith position in bitmask means the presence of nums[i]
+     * in the subset, and 0 means its absence.
+     *
+     * Return output list.
+     * @param nums
+     * @return
+     */
+    private static List<List<Integer>> subsetsUsingBitmasks(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int nthBit = 1 << (nums.length);
+
+        // generate bitmask, from 0..00 to 1..11
+        for ( int i = 0; i < nthBit; i++ )
+        {
+            // generate bitmask, from 0..00 to 1..11
+            String bitMask = Integer.toBinaryString( i | nthBit ).substring( 1 );
+
+            //
+            List<Integer> subset = new ArrayList<>();
+            for ( int j = 0; j < nums.length; j++ )
+            {
+                if ( bitMask.charAt( j ) == '1' ) subset.add( nums[j] );
+            }
+            result.add( subset );
+        }
+        return result;
     }
 }
