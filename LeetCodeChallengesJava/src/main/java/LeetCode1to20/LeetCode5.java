9bd4119d836f7dab4979eafa7dd2ef35b7ea3f12
Jangleur1991 <Jangleur@web.de>
2024-06-16 19:03:25 +0100

    WIP

---
+++ LeetCodeChallengesJava/src/main/java/LeetCode1to20/LeetCode5.java
@@ -0,0 +1,16 @@
+package LeetCode1to20;
+
+//https://leetcode.com/problems/longest-palindromic-substring/editorial/
+public class LeetCode5 {
+    public static void main(String[] args) {
+        String s1 = "babad";
+        String s2 = "cbbd";
+
+        System.out.println(longestPalindrome(s1));
+        System.out.println(longestPalindrome(s2));
+    }
+
+    private static String longestPalindrome(String s) {
+
+    }
+}
